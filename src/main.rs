3ba4da683033acb3586695dfdef8094202e906a4
lukasz.czaplinski <czapl.luk+git@gmail.com>
2017-09-14 21:28:43 +0200

    Read icons from Yaml files

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,14 @@ use clap::{Arg, App};
 extern crate termion;
 use termion::color;
 
+extern crate serde_derive;
+extern crate serde_yaml;
+
 use std::path;
 use std::env;
 use std::fs;
+
+use std::collections::HashMap;
 
 #[derive(Debug, PartialEq, Clone, Copy)]
 enum Verbosity {
@@ -16,9 +21,15 @@ enum Verbosity {
 }
 
 #[derive(Debug)]
+struct Icons {
+    file: HashMap<String, String>,
+}
+
+#[derive(Debug)]
 struct Action {
     verbosity: Verbosity,
     directory: path::PathBuf,
+    icons: Icons,
 }
 
 struct Attr {
@@ -26,70 +37,8 @@ struct Attr {
     color: color::AnsiValue,
 }
 
-// ai:       "\ue7b4"
-// android:  "\ue70e"
-// apple:    "\uf179"
-// audio:    "\uf001"
-// avro:     "\ue60b"
-// c:        "\ue61e"
-// clj:      "\ue768"
-// coffee:   "\uf0f4"
-// conf:     "\ue615"
-// cpp:      "\ue61d"
-// css:      "\ue749"
-// d:        "\ue7af"
-// dart:     "\ue798"
-// db:       "\uf1c0"
-// diff:     "\uf440"
-// doc:      "\uf1c2"
-// ebook:    "\ue28b"
-// epub:     "\ue28a"
-// erl:      "\ue7b1"
-// file:     "\uf15b"
-// font:     "\uf031"
-// gform:    "\uf298"
-// git:      "\uf1d3"
-// go:       "\ue626"
-// hs:       "\ue777"
-// html:     "\uf13b"
-// image:    "\uf1c5"
-// iml:      "\ue7b5"
-// java:     "\ue204"
-// js:       "\ue74e"
-// json:     "\ue60b"
-// less:     "\ue758"
-// lua:      "\ue620"
-// md:       "\uf48a"
-// mustache: "\ue60f"
-// npmignore: "\ue71e"
-// pdf:      "\uf1c1"
-// php:      "\ue73d"
-// pl:       "\ue769"
-// ppt:      "\uf1c4"
-// psd:      "\ue7b8"
-// py:       "\ue606"
-// r:        "\uf25d"
-// rb:       "\ue21e"
-// rdb:      "\ue76d"
-// rss:      "\uf09e"
-// scala:    "\ue737"
-// shell:    "\uf489"
-// sqlite3:  "\ue7c4"
-// styl:     "\ue600"
-// tex:      "\ue600"
-// ts:       "\ue628"
-// twig:     "\ue61c"
-// txt:      "\uf15c"
-// video:    "\uf03d"
-// vim:      "\ue62b"
-// windows:  "\uf17a"
-// xls:      "\uf1c3"
-// xml:      "\ue619"
-// yml:      "\uf481"
-// zip:      "\uf410"
-
-fn get_attr(_ : &path::Path) -> Attr {
-    return Attr { icon: String::from("\u{f17a}"), color: color::AnsiValue::rgb(2,2,5) }
+fn get_attr(icons : &Icons, _ : &path::Path) -> Attr {
+    return Attr { icon: icons.file.get("windows").unwrap().clone(), color: color::AnsiValue::rgb(2,2,5) }
 }
 
 fn run(action : Action) {
@@ -101,7 +50,7 @@ fn run(action : Action) {
 
     for dir in dirs {
         let path = dir.unwrap().path();
-        let attr = get_attr(&path);
+        let attr = get_attr(&action.icons, &path);
         let name = path.display();
         println!("{icon}  {color}{name}{reset}",
                  name = name,
@@ -131,10 +80,14 @@ fn main() {
         3 | _ => println!("Can't be more verbose!"),
     }
 
+    let file_icons = serde_yaml::from_str(include_str!("default_config/files.yaml")).unwrap();
     let cdir = env::current_dir().unwrap();
     let action = Action {
         verbosity: verbosity,
         directory: cdir,
+        icons: Icons {
+            file: file_icons,
+        },
     };
 
     if verbosity == Verbosity::Debug {
