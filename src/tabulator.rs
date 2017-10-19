13a88304a551f3c6cfcc1822dcc5b2ce29004d81
lukasz.czaplinski <czapl.luk+git@gmail.com>
2017-10-19 19:38:05 +0200

    Make binsearch generic

--- src/tabulator.rs
+++ src/tabulator.rs
@@ -116,8 +116,8 @@ impl Tabulator for PlanningTabulator {
     }
 }
 
-fn binsearch<F>(range : &Vec<i64>, pred : F) -> Option<usize>
-    where F: Fn(i64) -> bool {
+fn binsearch<T, F>(range : &Vec<T>, pred : F) -> Option<usize>
+    where F: Fn(&T) -> bool {
     let mut slice = &range[..];
     let mut index = 0;
     let mut counter = range.len();
@@ -129,7 +129,7 @@ fn binsearch<F>(range : &Vec<i64>, pred : F) -> Option<usize>
         assert!(counter > 0);
 
         let mid = slice.len() / 2;
-        let mid_value = slice[mid];
+        let mid_value = &slice[mid];
         if pred(mid_value) {
             // Go left
             slice = &slice[0..mid+1]
@@ -139,8 +139,8 @@ fn binsearch<F>(range : &Vec<i64>, pred : F) -> Option<usize>
             slice = &slice[mid..]
         }
         if slice.len() < 3 {
-            if pred(slice[0]) { return Some(index) }
-            if slice.len() == 2 && pred(slice[1]) { return Some(index+1) }
+            if pred(&slice[0]) { return Some(index) }
+            if slice.len() == 2 && pred(&slice[1]) { return Some(index+1) }
             return None
         }
     }
@@ -151,28 +151,28 @@ mod binsearch_tests {
     use super::*;
     #[test]
     fn for_simple_case() {
-        assert_eq!(Some(1), binsearch(&vec![0,1,2], |i| i >= 1))
+        assert_eq!(Some(1), binsearch(&vec![0,1,2], |i| *i >= 1))
     }
 
     #[test]
     fn when_nothing_matches() {
-        assert_eq!(None, binsearch(&vec![0,1,2], |i| i >= 3))
+        assert_eq!(None, binsearch(&vec![0,1,2], |i| *i >= 3))
     }
 
     #[test]
     fn when_array_is_small() {
-        assert_eq!(Some(1), binsearch(&vec![0,1], |i| i >= 1));
-        assert_eq!(Some(0), binsearch(&vec![0,1], |i| i >= 0))
+        assert_eq!(Some(1), binsearch(&vec![0,1], |i| *i >= 1));
+        assert_eq!(Some(0), binsearch(&vec![0,1], |i| *i >= 0))
     }
 
     #[test]
     fn when_array_is_big() {
-        assert_eq!(Some(11), binsearch(&vec![0,1,2,3,4,5,6,7,8,9,10,11,12,13,14], |i| i >= 11));
+        assert_eq!(Some(11), binsearch(&vec![0,1,2,3,4,5,6,7,8,9,10,11,12,13,14], |i| *i >= 11));
     }
 
     #[test]
     fn when_array_is_empty() {
-        assert_eq!(None, binsearch(&vec![], |i| i >= 11));
+        assert_eq!(None, binsearch(&(vec![] as Vec<i64>), |i| *i >= 11));
     }
 }
 
@@ -185,7 +185,7 @@ impl Tabulator for BinsearchTabulator {
         let min_rows = (config.max_width / (width + 1)) as i64;
         let max_rows = (config.max_width / MIN_FORMAT_ENTRY_LENGTH) as i64;
         let range : Vec<i64> = range_step(max_rows, min_rows, -1).collect();
-        let row_cap = if let Some(idx) = binsearch(&range, |row_cap| is_valid_as_rows(config, &names, row_cap as usize)) {
+        let row_cap = if let Some(idx) = binsearch(&range, |row_cap| is_valid_as_rows(config, &names, *row_cap as usize)) {
             range[idx]
         } else {
             1
