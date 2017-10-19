cfd14346f742ce383d406239d4228cdd566afe89
lukasz.czaplinski <czapl.luk+git@gmail.com>
2017-10-19 20:10:07 +0200

    Reduce allocations for tabulators

--- src/tabulator.rs
+++ src/tabulator.rs
@@ -31,17 +31,23 @@ fn as_rows<T : Clone>(names : &Vec<T>, row_cap : usize) -> Vec<Vec<T>> {
     rows
 }
 
+type ColumnSetup = Vec<usize>;
+
+fn is_valid(out : &ColumnSetup, max_width : usize) -> bool {
+    let mut width = 0;
+    for c in out { width += *c }
+    return width < max_width
+}
+
 // NOTE: Assumes out has same-sized rows
-fn is_valid(out : Vec<Vec<usize>>, max_width : usize) -> bool {
+fn column_setup(out : Vec<Vec<usize>>) -> ColumnSetup {
     let mut col_widths = vec![0; out[0].len()];
     for r in out {
         for (i, s) in r.iter().enumerate() {
             col_widths[i] = max(col_widths[i], *s);
         }
     }
-    let mut width = 0;
-    for c in col_widths { width += c }
-    return width < max_width
+    col_widths
 }
 
 #[cfg(test)]
@@ -49,34 +55,35 @@ mod is_valid_tests {
     use super::*;
     #[test]
     fn for_simple_case() {
-        assert_eq!(false, is_valid(vec![vec![1,2], vec![2,1]], 2))
+        assert_eq!(false, is_valid(&column_setup(vec![vec![1,2], vec![2,1]]), 2))
     }
 
     #[test]
     fn when_total_col_width_exceeds_max() {
-        assert_eq!(false, is_valid(vec![vec![1,3], vec![3,1]], 5))
+        assert_eq!(false, is_valid(&column_setup(vec![vec![1,3], vec![3,1]]), 5))
     }
 
     #[test]
     fn when_fits() {
-        assert_eq!(true, is_valid(vec![vec![1,2], vec![1,1]], 4))
+        assert_eq!(true, is_valid(&column_setup(vec![vec![1,2], vec![1,1]]), 4))
     }
 }
 
-fn is_valid_as_rows(config: &Config, names : &Vec<Entry>, row_cap : usize) -> bool {
-    is_valid(as_rows(&names.iter().map(|e| config.formatter.predict(e)).collect(), row_cap), config.max_width)
+fn predict_column_setup(config: &Config, names : &Vec<Entry>, row_cap : usize) -> ColumnSetup {
+    column_setup(as_rows(&names.iter().map(|e| config.formatter.predict(e)).collect(), row_cap))
+}
+
+fn is_valid_as_rows(config: &Config, names : &Vec<Entry>, row_cap : usize) -> Option<ColumnSetup> {
+    let setup = predict_column_setup(config, names, row_cap);
+    if is_valid(&setup, config.max_width) {
+        Some(setup)
+    } else { None }
 }
 
 // NOTE: Assumes names has same-sized rows
-fn format_as_rows(config : &Config, names : &Vec<Entry>, row_cap : usize) -> Output {
+fn format_as_rows(config : &Config, names : &Vec<Entry>, col_widths : ColumnSetup) -> Output {
+    let row_cap = col_widths.len();
     let rows = as_rows(names, row_cap);
-    let mut col_widths = vec![0; rows[0].len()];
-    for r in &rows {
-        for (i, s) in r.iter().enumerate() {
-            let predicted = config.formatter.predict(s);
-            col_widths[i] = max(col_widths[i],predicted)
-        }
-    }
     let entry_configs : Vec<EntryConfig> = col_widths.iter().map(|width| EntryConfig{width: *width, ..config.entry.clone()}).collect();
     let mut out = Vec::with_capacity(names.len());
     for r in rows {
@@ -105,14 +112,14 @@ pub struct PlanningTabulator;
 impl Tabulator for PlanningTabulator {
     fn tabulate(&self, config : &Config, names : Vec<Entry>) -> Output {
         let width = max_width(config, &names);
-        let min_rows = (config.max_width / (width + 1)) as i64;
+        let min_rows = max((config.max_width / (width + 1)) as i64, 1);
         let max_rows = (config.max_width / MIN_FORMAT_ENTRY_LENGTH) as i64;
         for row_cap in range_step(max_rows, min_rows, -1) {
-            if is_valid_as_rows(config, &names, row_cap as usize) {
-                return format_as_rows(config, &names, row_cap as usize)
+            if let Some(column_setup) = is_valid_as_rows(config, &names, row_cap as usize) {
+                return format_as_rows(config, &names, column_setup)
             }
         }
-        format_as_rows(config, &names, min_rows as usize)
+        format_as_rows(config, &names, predict_column_setup(config, &names, 1))
     }
 }
 
@@ -182,15 +189,16 @@ pub struct BinsearchTabulator;
 impl Tabulator for BinsearchTabulator {
     fn tabulate(&self, config : &Config, names : Vec<Entry>) -> Output {
         let width = max_width(config, &names);
-        let min_rows = (config.max_width / (width + 1)) as i64;
+        // NOTE: Cannot use usize, due to negative range.
+        let min_rows = max((config.max_width / (width + 1)) as i64, 1);
         let max_rows = (config.max_width / MIN_FORMAT_ENTRY_LENGTH) as i64;
         let range : Vec<i64> = range_step(max_rows, min_rows, -1).collect();
-        let row_cap = if let Some(idx) = binsearch(&range, |row_cap| is_valid_as_rows(config, &names, *row_cap as usize)) {
+        let row_cap = if let Some(idx) = binsearch(&range, |row_cap| is_valid_as_rows(config, &names, *row_cap as usize).is_some()) {
             range[idx]
         } else {
             1
         };
-        format_as_rows(config, &names, row_cap as usize)
+        format_as_rows(config, &names, predict_column_setup(config, &names, row_cap as usize))
     }
 }
 
@@ -199,7 +207,7 @@ pub struct NaiveTabulator;
 impl Tabulator for NaiveTabulator {
     fn tabulate(&self, config : &Config, names : Vec<Entry>) -> Output {
         let width = max_width(config, &names) + 2;
-        let rows = config.max_width / width;
-        format_as_rows(config, &names, rows)
+        let rows = max(config.max_width / width, 1);
+        format_as_rows(config, &names, predict_column_setup(config, &names, rows as usize))
     }
 }
