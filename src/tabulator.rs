61038e64f8ec7f27638373ea8441717267ee3b64
lukasz.czaplinski <czapl.luk+git@gmail.com>
2017-10-20 18:50:03 +0200

    Simplify & fix var naming
    Simpler:
    - row_cap -> n_cols
    Fix:
    - min_rows -> min_cols
    - max_rows -> max_cols

--- src/tabulator.rs
+++ src/tabulator.rs
@@ -79,12 +79,12 @@ mod is_valid_tests {
     }
 }
 
-fn predict_column_setup(config: &Config, names : &Vec<Entry>, row_cap : usize) -> ColumnSetup {
-    column_setup(as_rows(&names.iter().map(|e| config.formatter.predict(e)).collect(), row_cap))
+fn predict_column_setup(config: &Config, names : &Vec<Entry>, n_cols : usize) -> ColumnSetup {
+    column_setup(as_rows(&names.iter().map(|e| config.formatter.predict(e)).collect(), n_cols))
 }
 
-fn is_valid_as_rows(config: &Config, names : &Vec<Entry>, row_cap : usize) -> Option<ColumnSetup> {
-    let setup = predict_column_setup(config, names, row_cap);
+fn is_valid_as_rows(config: &Config, names : &Vec<Entry>, n_cols : usize) -> Option<ColumnSetup> {
+    let setup = predict_column_setup(config, names, n_cols);
     if is_valid(&setup, config.max_width) {
         Some(setup)
     } else { None }
@@ -92,8 +92,8 @@ fn is_valid_as_rows(config: &Config, names : &Vec<Entry>, row_cap : usize) -> Op
 
 // NOTE: Assumes names has same-sized rows
 fn format_as_rows(config : &Config, names : &Vec<Entry>, col_widths : ColumnSetup) -> Output {
-    let row_cap = col_widths.len();
-    let rows = as_rows(names, row_cap);
+    let n_cols = col_widths.len();
+    let rows = as_rows(names, n_cols);
     let entry_configs : Vec<EntryConfig> = col_widths.iter().map(|width| EntryConfig{width: *width, ..config.entry.clone()}).collect();
     let mut out = Vec::with_capacity(names.len());
     for r in rows {
@@ -101,7 +101,7 @@ fn format_as_rows(config : &Config, names : &Vec<Entry>, col_widths : ColumnSetu
             out.push(config.formatter.format(&entry_configs[i], s));
         }
     }
-    as_rows(&out, row_cap)
+    as_rows(&out, n_cols)
 }
 
 fn max_width(config : &Config, names : &Vec<Entry>) -> usize {
@@ -122,10 +122,10 @@ pub struct PlanningTabulator;
 impl Tabulator for PlanningTabulator {
     fn tabulate(&self, config : &Config, names : Vec<Entry>) -> Output {
         let width = max_width(config, &names);
-        let min_rows = max((config.max_width / (width + 1)) as i64, 1);
-        let max_rows = (config.max_width / MIN_FORMAT_ENTRY_LENGTH) as i64;
-        for row_cap in range_step(max_rows, min_rows, -1) {
-            if let Some(column_setup) = is_valid_as_rows(config, &names, row_cap as usize) {
+        let min_cols = max((config.max_width / (width + 1)) as i64, 1);
+        let max_cols = (config.max_width / MIN_FORMAT_ENTRY_LENGTH) as i64;
+        for n_cols in range_step(max_cols, min_cols, -1) {
+            if let Some(column_setup) = is_valid_as_rows(config, &names, n_cols as usize) {
                 return format_as_rows(config, &names, column_setup)
             }
         }
@@ -200,15 +200,15 @@ impl Tabulator for BinsearchTabulator {
     fn tabulate(&self, config : &Config, names : Vec<Entry>) -> Output {
         let width = max_width(config, &names);
         // NOTE: Cannot use usize, due to negative range.
-        let min_rows = max((config.max_width / (width + 1)) as i64, 1);
-        let max_rows = (config.max_width / MIN_FORMAT_ENTRY_LENGTH) as i64;
-        let range : Vec<i64> = range_step(max_rows, min_rows, -1).collect();
-        let row_cap = if let Some(idx) = binsearch(&range, |row_cap| is_valid_as_rows(config, &names, *row_cap as usize).is_some()) {
+        let min_cols = max((config.max_width / (width + 1)) as i64, 1);
+        let max_cols = (config.max_width / MIN_FORMAT_ENTRY_LENGTH) as i64;
+        let range : Vec<i64> = range_step(max_cols, min_cols, -1).collect();
+        let n_cols = if let Some(idx) = binsearch(&range, |n_cols| is_valid_as_rows(config, &names, *n_cols as usize).is_some()) {
             range[idx]
         } else {
             1
         };
-        format_as_rows(config, &names, predict_column_setup(config, &names, row_cap as usize))
+        format_as_rows(config, &names, predict_column_setup(config, &names, n_cols as usize))
     }
 }
 
